cdc03e062abcf63d83aa4132603f65c0e962373d
Chris Gerlach <gerlach4senate@gmail.com>
2023-03-13 22:17:25 -0500

    Added building instantiations
    Completed half of buildings

--- code.py
+++ code.py
@@ -24,11 +24,21 @@ class Building:
         return self.position
 
 # Instantiate Buildings
-R25 = Building("BlueHouse", "Fireplace", 1)
-B13 = Building("MusicShoppe", "Lit", 2)
-B5 = Building("CigarShoppe", "Lit", 3)
-B2 = Building("BlackForestCafe", "Lit", 4)
-R26 = Building("GreyHouse", "Fireplace", 5)
+B_BKY = Building("Bakery", "Fireplace", 1)
+B_BFC = Building("BlackForestCafe", "Lit", 2)
+B_CWA = Building("CaddingtonWinterAle", "Lit", 3)
+B_CDS = Building("CandyShoppe", "Lit", 4)
+B_CGS = Building("CigarShoppe", "Fireplace", 5)
+C_BWS = Building("Berrywood School", "Lit", 6)
+C_CHU = Building("Church", "Lit", 7)
+C_FST = Building("FireStation", "Lit", 8)
+C_GZB = Building("Gazebo", "Lit", 9)
+C_LHS = Building("Lighthouse", "Lit", 10)
+R_HSB = Building("HorseBarn", "Lit", 11)
+R_HS1 = Building("House1", "Lit", 12)
+R_HS2 = Building("House2", "Lit", 13)
+R_HS3 = Building("House3", "Lit", 14)
+R_HS4 = Building("House4", "Lit", 15)
 
 # Initialize Neopixel Parameters
 pixelPin = board.D10
@@ -59,8 +69,13 @@ def BlinkPattern(index):
         time.sleep(1)
 
 # LitPattern(B2.position)
+<<<<<<< Updated upstream
 BlinkPattern(B2.position + 1)
 BlinkPattern(B2.position)
+=======
+BlinkPattern(B_BFC.position + 1)
+BlinkPattern(B_BFC.position)
+>>>>>>> Stashed changes
 
 # if __name__ == '__main__':
 # Process(target=BlinkPattern).start()
