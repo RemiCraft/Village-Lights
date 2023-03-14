334cb7827b7aec281cb3a9e7f1c8ecf545be6aef
RemiCraft <1030gman@gmail.com>
2023-03-13 21:48:30 -0500

    Added Patterns
    Blink & Lit

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import board
 import time
 import neopixel
 # from rainbowio import colorwheel
+# from multiprocessing import Process
 
 class Building:
     def __init__(self, name, pattern, position):
@@ -41,12 +42,29 @@ GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
+OFF = (0, 0, 0)
 
-while True:
-    print("running")
-    for i in range(numPixels):
-        print(i)
-        pixels[i] = (255, 0, 0)
+def LitPattern(index):
+    while 1:
+        pixels[index] = (255, 255, 255)
+
+def BlinkPattern(index):
+    while True:
+        print("loop")
+        pixels[index] = (255, 0, 0)
         pixels.show()
-    time.sleep(5)
+        time.sleep(1)
+        pixels[index] = OFF
+        pixels.show()
+        time.sleep(1)
+
+# LitPattern(B2.position)
+BlinkPattern(B2.position + 1)
+BlinkPattern(B2.position)
+
+# if __name__ == '__main__':
+# Process(target=BlinkPattern).start()
+# Process(target=BlinkPattern2).start()
+
+
 
