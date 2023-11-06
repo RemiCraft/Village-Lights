bea6dd305a7d57be14d5f432594cff61547c086e
RemiCraft <1030gman@gmail.com>
2023-11-06 16:51:13 -0600

    Moved Light Patterns To Lists

--- Lights/Lights.ino
+++ Lights/Lights.ino
@@ -1,11 +1,16 @@
 #include <Arduino_FreeRTOS.h>
 #include <FastLED.h>
+#include <List.hpp>
 #include "Building.h"
 
 void TestPattern( void *pvParameters );
 void BlinkPattern( void *pvParameters );
 void FastBlinkPattern( void *pvParameters );
 void FireplacePattern( void *pvParameters );
+List<Building> fireplaceBuildings;
+List<Building> fastBlinkBuildings;
+List<Building> blinkBuildings;
+List<Building> newBlinkBuildings;
 
 #define NUM_LEDS 5
 #define DATA_PIN 5
@@ -18,11 +23,11 @@ void FireplacePattern( void *pvParameters );
 CRGB leds[NUM_LEDS];
 // Building("name", position, patternID)
 Building buildings[] = {
-  Building("house1", 0, 0),
-  Building("house2", 1, 1),
-  Building("house3", 2, 0),
-  Building("house4", 3, 1),
-  Building("house5", 4, 0),
+  Building("house1", 0, 3),
+  Building("house2", 1, 3),
+  Building("house3", 2, 3),
+  Building("house4", 3, 3),
+  Building("house5", 4, 3),
 };
 
 void setup()
@@ -33,9 +38,33 @@ void setup()
 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 
+  for (int i = 0; i < NUM_LEDS; i++)
+    {
+      if (buildings[i].GetPattern() == 0)
+      {
+        blinkBuildings.add(buildings[i]);
+      }
+
+      if (buildings[i].GetPattern() == 1)
+      {
+        fastBlinkBuildings.add(buildings[i]);
+      }
+
+      if (buildings[i].GetPattern() == 2)
+      {
+        fireplaceBuildings.add(buildings[i]);
+      }
+
+      if (buildings[i].GetPattern() == 3)
+      {
+        newBlinkBuildings.add(buildings[i]);
+      }
+    }
+
   xTaskCreate(TaskBlink, "BlinkPattern", 128, NULL, 2, NULL);
   xTaskCreate(TaskFastBlink, "FastBlinkPattern", 128, NULL, 2, NULL);
   xTaskCreate(Fireplace, "FireplacePattern", 128, NULL, 2, NULL);
+  xTaskCreate(TaskNewBlink, "NewBlinkPattern", 128, NULL, 2, NULL);
   //xTaskCreate(TaskTest, "TestPattern", 128, NULL, 2, NULL);
 }
 
@@ -109,7 +138,28 @@ void TaskFastBlink(void *pvParameters)
     }
     vTaskDelay( 250 / portTICK_PERIOD_MS );
   }
-}        
+}
+
+void TaskNewBlink(void *pvParameters)
+{
+  (void) pvParameters;
+
+  for (;;)
+  {
+    for(int i = 0; i < newBlinkBuildings.getSize(); i++)
+    {
+      leds[i] = CRGB::Red;
+      FastLED.show();
+    }
+    vTaskDelay( 500 / portTICK_PERIOD_MS );
+    for(int i = 0; i < newBlinkBuildings.getSize(); i++)
+    {
+      leds[i] = CRGB::Black;
+      FastLED.show();
+    }
+    vTaskDelay( 500 / portTICK_PERIOD_MS );
+  }
+}
 
 void Fireplace(void *pvParameters)
 {
